161058271936554fbc480fecf765e101c9ac8f4e
Guy Chalk <guychalk532@gmail.com>
2022-08-25 19:43:05 +0100

    parentheses lab for github

---
+++ parentheses_checker_start/src/main/java/ParenthesesChecker.java
@@ -0,0 +1,28 @@
+public class ParenthesesChecker {
+
+    // start by creating a simple method that takes in a string
+    // and go from there
+
+        public String testString;
+        public boolean checkParentheses;
+
+    // creating my getters + setters below:
+
+        public void setPart1(String testString) {
+            this.testString = testString;
+        }
+
+         public String getTestString() {
+            return this.testString;
+         }
+
+    public boolean checkParentheses(String testString) {
+            return true;
+    }
+
+
+
+         public boolean isCheckParentheses() {
+        return checkParentheses;
+        }
+}
